00b29a9996512633adf1ac77249c2985bbfe3c98
Tran-Mai-Anh <155942841+Tran-Mai-Anh@users.noreply.github.com>
2025-04-11 15:44:16 +0700

    push

---
+++ Cosmetic-ecommerce-website-main/Cosmetic/Models/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace Shop.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Address { get; set; }
+
+        [StringLength(10)]
+        public string PhoneNumber { get; set; }
+
+        public string Status { get; set; } = "active";
+    }
+}
